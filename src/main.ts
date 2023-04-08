665b9329c9bf2b8a1b615d169d2a6164fb58f244
MrQuentinet <quentinvilluis@gmail.com>
2023-04-08 00:51:07 -0400

    Create the action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {PushEvent} from '@octokit/webhooks-definitions/schema'
+import {Cargo} from '@rinse-repeat/actions-rs-core'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+export async function run(): Promise<void> {
+  const cargo = await Cargo.get()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  if (github.context.eventName === 'push') {
+    const pushPayload = github.context.payload as PushEvent
+    const commit = pushPayload.head_commit
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (commit?.message?.startsWith('major:')) {
+      await cargo.call(['bump', 'major'])
+    } else if (commit?.message?.startsWith('minor:')) {
+      await cargo.call(['bump', 'minor'])
+    } else if (commit?.message?.startsWith('fix:')) {
+      await cargo.call(['bump'])
+    }
   }
 }
 
-run()
+async function main(): Promise<void> {
+  try {
+    await run()
+  } catch (error) {
+    core.setFailed((<Error>error).message)
+  }
+}
+
+void main()
